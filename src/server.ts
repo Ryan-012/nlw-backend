5391f0027a488a1123d1d21de5f4603d1bea9d81
ryan aragao <ryanaragao012@gmail.com>
2023-05-22 21:54:33 -0300

    five lesson

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,19 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 const app = fastify()
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(multipart)
 
 app.register(cors, {
   origin: true,
@@ -15,6 +25,7 @@ app.register(jwt, {
 app.register(authRoutes)
 
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
