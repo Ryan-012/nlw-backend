8369bd6d10e6c32bb5463933ac11d83b9a97ef7d
ryan aragao <ryanaragao012@gmail.com>
2023-06-02 13:00:51 -0300

    created useInfo route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
+import { z } from 'zod'
 // import { z } from 'zod'
+
 export async function UserRoutes(app: FastifyInstance) {
   app.addHook('preHandler', async (req) => {
     await req.jwtVerify()
@@ -12,5 +14,34 @@ export async function UserRoutes(app: FastifyInstance) {
     })
 
     return users
+  })
+
+  app.get('/users/:id', async (req, res) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(req.params)
+
+    const user = await prisma.user.findUnique({
+      where: { id },
+      include: {
+        memories: {
+          where: {
+            isPublic: true,
+          },
+          orderBy: {
+            createdAt: 'asc',
+          },
+          include: {
+            likes: true,
+          },
+        },
+      },
+    })
+
+    if (!user) return res.callNotFound()
+
+    return user
   })
 }
