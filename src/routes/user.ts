4e2d6c63389af6138e53d741b9c9358a8afb84ce
ryan aragao <ryanaragao012@gmail.com>
2023-06-06 20:16:54 -0300

    finishing project

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { z } from 'zod'
-// import { z } from 'zod'
 
 export async function UserRoutes(app: FastifyInstance) {
   app.addHook('preHandler', async (req) => {
